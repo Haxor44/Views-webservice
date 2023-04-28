4fbec923f819c9cc489898ddf6b2bacda9522380
Haxor44 <50330948+Haxor44@users.noreply.github.com>
2023-04-28 16:52:47 +0300

    Update SecurityConfig.java

--- src/main/java/com/roman/Views/security/SecurityConfig.java
+++ src/main/java/com/roman/Views/security/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 WebMvcConfigurer.super.addCorsMappings(registry);
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("https://roman-frontend.vercel.app/")
                         .allowedMethods("*")
                         .allowCredentials(true);
 
