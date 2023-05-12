d77ef8f3cfff689f5f668c9ec1fb87c2e074adf0
Haxor44 <50330948+Haxor44@users.noreply.github.com>
2023-05-12 12:46:42 +0300

    Update ViewsController.java

--- src/main/java/com/roman/Views/controller/ViewsController.java
+++ src/main/java/com/roman/Views/controller/ViewsController.java
@@ -8,9 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "Requestor-Type", exposedHeaders = "X-Get-Header")
 @RestController
 @RequestMapping("/api/v1/services")
 @RequiredArgsConstructor
