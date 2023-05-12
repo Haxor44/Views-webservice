c96faf429503d5dddb877d986140f96b65c969ac
Haxor44 <50330948+Haxor44@users.noreply.github.com>
2023-05-12 11:07:00 +0300

    Update SecurityConfig.java

--- src/main/java/com/roman/Views/security/SecurityConfig.java
+++ src/main/java/com/roman/Views/security/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 WebMvcConfigurer.super.addCorsMappings(registry);
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000/")
+                        .allowedOrigins("http://localhost:3000/services")
                         .allowedMethods("*")
                         .allowCredentials(true);
 
